c055ff8830fd4f1faf9affb8d609215eff22a394
UniversoParalelo <cmartinrodich@gmail.com>
2024-09-19 14:24:07 -0300

    adding some feature for login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,9 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation'; 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -81,11 +83,31 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   try{
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
   }catch(error){
     return {message: 'Error deleting invoice'};
+  }
+}
+
+
+ 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
